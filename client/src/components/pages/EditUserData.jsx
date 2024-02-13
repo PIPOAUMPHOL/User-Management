2dad7e0c4842aa40c4b1dcf19b7a0946dd8cb3f8
PIPOAUMPHOL <aumpholllo@gmail.com>
2024-02-13 23:35:43 +0700

    fix: birthday format

--- client/src/components/pages/EditUserData.jsx
+++ client/src/components/pages/EditUserData.jsx
@@ -73,7 +73,7 @@ function EditUserData() {
     const dd = String(today.getDate()).padStart(2, "0");
     const mm = String(today.getMonth() + 1).padStart(2, "0");
     const yyyy = today.getFullYear();
-    return `${yyyy}-${mm}-${dd}`;
+    return `${dd}/${mm}/${yyyy}`;
   }
 
   return (
